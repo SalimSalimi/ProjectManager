bd56c5e9c64aed712ee77b8e950262bd7c8c0cdc
Salim Salimi <salim.salimi@protonmail.com>
2020-11-24 14:23:52 +0100

    fix remove unnecessary import

--- app/src/main/java/fr/im/salimi/projectmanager/ui/developerEdit/DeveloperEditViewModel.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/developerEdit/DeveloperEditViewModel.kt
@@ -1,6 +1,5 @@
 package fr.im.salimi.projectmanager.ui.developerEdit
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
