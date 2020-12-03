72c2cebd3531daa8343b68d576619e8d0219662c
Salim Salimi <salim.salimi@protonmail.com>
2020-12-03 17:03:35 +0100

    Add a new button to delete

--- app/src/main/java/fr/im/salimi/projectmanager/ui/developerList/RecyclerClickListenersCallback.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/developerList/RecyclerClickListenersCallback.kt
@@ -2,6 +2,10 @@ package fr.im.salimi.projectmanager.ui.developerList
 
 import fr.im.salimi.projectmanager.data.entities.Developer
 
-class RecyclerClickListenersCallback(private val onLongClickCallback: (Long) -> Unit ) {
+class RecyclerClickListenersCallback(
+    private val onLongClickCallback: (Long) -> Unit,
+    /*private val onDeleteClickCallback: (Developer) -> Unit*/) {
+
     fun onLongClick(developer: Developer) = onLongClickCallback(developer.id)
+    //fun onDeleteClick(developer: Developer) = onDeleteClickCallback(developer)
 }
