ef4287fe0891db93c9d23c530c2498de35c0422b
Salim Salimi <salim.salimi@protonmail.com>
2020-12-12 23:52:21 +0100

    Rewrite TeamRepository to inherit from BaseRepository

--- app/src/main/java/fr/im/salimi/projectmanager/data/repositories/DeveloperRepository.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/data/repositories/DeveloperRepository.kt
@@ -4,13 +4,12 @@ import fr.im.salimi.projectmanager.data.daos.DeveloperDao
 import fr.im.salimi.projectmanager.data.entities.Developer
 import kotlinx.coroutines.flow.Flow
 
-class DeveloperRepository(private val developerDao: DeveloperDao): BaseRepository<Developer>(developerDao) {
+class DeveloperRepository(private val developerDao: DeveloperDao) : BaseRepository<Developer>(developerDao) {
 
-    override suspend fun getById(id: Long): Developer {
-        return developerDao.getById(id)
-    }
+    override suspend fun getById(id: Long): Developer =
+            developerDao.getById(id)
 
-    override fun getAll(): Flow<List<Developer>> {
-        return developerDao.getAll()
-    }
+    override fun getAll(): Flow<List<Developer>> =
+            developerDao.getAll()
+
 }
