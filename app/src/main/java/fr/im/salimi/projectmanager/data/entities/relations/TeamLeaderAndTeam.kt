3256ed991da38b2a1dd25aefeb910ae2deb4a781
Salim Salimi <salim.salimi@protonmail.com>
2020-12-12 17:13:20 +0100

    create a new data class that represents One-To-One relation between Team and Developer for leaderId

---
+++ app/src/main/java/fr/im/salimi/projectmanager/data/entities/relations/TeamLeaderAndTeam.kt
@@ -0,0 +1,20 @@
+package fr.im.salimi.projectmanager.data.entities.relations
+
+import androidx.room.Embedded
+import androidx.room.Relation
+import fr.im.salimi.projectmanager.data.entities.Developer
+import fr.im.salimi.projectmanager.data.entities.Team
+
+/**
+ * One-To-One relation between leaderId of Team
+ * With Developer id
+ * */
+
+data class TeamLeaderAndTeam (
+    @Embedded
+    val teamLeader: Developer,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "leaderId"
+    )
+    val team: Team )
