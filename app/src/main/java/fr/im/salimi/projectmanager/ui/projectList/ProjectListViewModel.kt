ad1f52b95d0fdf137e56de7e65234f15899019e9
Salim Salimi <salim.salimi@protonmail.com>
2020-12-22 18:53:18 +0100

    Implemeting ProjectListViewModel to get projects list from repository

--- app/src/main/java/fr/im/salimi/projectmanager/ui/projectList/ProjectListViewModel.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/projectList/ProjectListViewModel.kt
@@ -1,7 +1,15 @@
 package fr.im.salimi.projectmanager.ui.projectList
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import fr.im.salimi.projectmanager.data.entities.Project
+import fr.im.salimi.projectmanager.data.repositories.ProjectRepository
 
-class ProjectListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ProjectListViewModel(repository: ProjectRepository) : ViewModel() {
+
+    private val _projectsList = repository.getAll()
+    val projectsList: LiveData<List<Project>>
+        get() = _projectsList.asLiveData()
+
 }
