14c75f7e5b99ed7b46793a21ff3cbb19e7652e90
Salim Salimi <salim.salimi@protonmail.com>
2020-12-22 19:14:07 +0100

    Implementing the ProjectListFragment

--- app/src/main/java/fr/im/salimi/projectmanager/ui/projectList/ProjectListFragment.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/projectList/ProjectListFragment.kt
@@ -6,21 +6,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
 import fr.im.salimi.projectmanager.R
+import fr.im.salimi.projectmanager.data.entities.Project
+import fr.im.salimi.projectmanager.databinding.ProjectListFragmentBinding
 
 class ProjectListFragment : Fragment() {
 
     private lateinit var viewModel: ProjectListViewModel
+    private lateinit var projectsList: List<Project>
+    private lateinit var binding: ProjectListFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.project_list_fragment, container, false)
+                              savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.project_list_fragment, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ProjectListViewModel::class.java)
-        // TODO: Use the ViewModel
+
     }
 
+    private fun initObservers() {
+        viewModel.projectsList.observe(viewLifecycleOwner) {
+            projectsList = it
+        }
+    }
+
+    private fun initAdapter() {
+        binding.projectsList.apply {
+            layoutManager = GridLayoutManager(requireContext(), 2)
+            val myAdapter = ProjectListAdapter(projectsList)
+            this.adapter = myAdapter
+        }
+    }
 }
