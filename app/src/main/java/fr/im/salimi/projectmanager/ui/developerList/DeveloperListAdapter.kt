e95517f8dbc01ac3b90b423398790a0ecc0d3fb1
Salim Salimi <salim.salimi@protonmail.com>
2020-12-25 13:54:41 +0100

    Refactor DeveloperListAdapter and DeveloperFragment to use the new BaseAdapter

--- app/src/main/java/fr/im/salimi/projectmanager/ui/developerList/DeveloperListAdapter.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/developerList/DeveloperListAdapter.kt
@@ -1,50 +1,15 @@
 package fr.im.salimi.projectmanager.ui.developerList
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
 import fr.im.salimi.projectmanager.R
 import fr.im.salimi.projectmanager.data.entities.Developer
 import fr.im.salimi.projectmanager.databinding.DeveloperListItemBinding
-import fr.im.salimi.projectmanager.ui.developerList.DeveloperListAdapter.ViewHolder.Companion.create
+import fr.im.salimi.projectmanager.ui.uiUtils.BaseAdapter
 
-class DeveloperListAdapter(private val clickListeners: RecyclerClickListenersCallback): RecyclerView.Adapter<DeveloperListAdapter.ViewHolder>() {
+class DeveloperListAdapter(var developersList: List<Developer>, private val clickListeners: DeveloperAdapterListener): BaseAdapter<Developer, DeveloperListItemBinding>(developersList, R.layout.developer_list_item){
 
-    var developers: List<Developer> = ArrayList()
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return create(parent)
+    override fun bind(item: Developer, binding: DeveloperListItemBinding) {
+        binding.developer = item
+        binding.listener = clickListeners
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val currentDeveloper = developers[position]
-        holder.bind(currentDeveloper, clickListeners)
-    }
-
-    override fun getItemCount() =
-            developers.size
-
-    class ViewHolder(private val binding: DeveloperListItemBinding): RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(developer: Developer, clickListeners: RecyclerClickListenersCallback) {
-            binding.developer = developer
-            binding.developerCard.setOnLongClickListener {
-                clickListeners.onLongClick(developer)
-                true
-            }
-            binding.deleteDeveloperBtn.setOnClickListener {
-                clickListeners.onDeleteClick(developer)
-            }
-        }
-
-        companion object {
-            fun create(parent: ViewGroup): ViewHolder {
-                val inflater = LayoutInflater.from(parent.context)
-                val binding: DeveloperListItemBinding = DeveloperListItemBinding
-                        .inflate(inflater, parent, false)
-                return ViewHolder(binding)
-            }
-        }
-    }
 }
