746ae8b55f1edd90043ffa6edbc72e73e2a4a8e4
Salim Salimi <salim.salimi@protonmail.com>
2020-12-26 16:59:27 +0100

    Fix useless imports

--- app/src/main/java/fr/im/salimi/projectmanager/ui/projectList/ProjectListViewModel.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/projectList/ProjectListViewModel.kt
@@ -1,6 +1,5 @@
 package fr.im.salimi.projectmanager.ui.projectList
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -24,7 +23,6 @@ class ProjectListViewModel(repository: ProjectRepository) : ViewModel() {
 
     fun onAddBtnClicked() {
         navigateToProjectForm()
-        Log.d("ProjectListViewModel", "CLICKED")
     }
 
     private fun navigateToProjectForm() {
