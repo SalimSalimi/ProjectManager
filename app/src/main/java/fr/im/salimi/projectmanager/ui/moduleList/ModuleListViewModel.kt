c5b8a31bf95cfdbe926cd9a173c4d025ee7822a8
Salim Salimi <salim.salimi@protonmail.com>
2020-12-29 00:03:01 +0100

    Implement ModuleViewModel to get list of modules from repository

--- app/src/main/java/fr/im/salimi/projectmanager/ui/moduleList/ModuleListViewModel.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/moduleList/ModuleListViewModel.kt
@@ -1,7 +1,15 @@
 package fr.im.salimi.projectmanager.ui.moduleList
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import fr.im.salimi.projectmanager.data.entities.Module
+import fr.im.salimi.projectmanager.data.repositories.ModuleRepository
 
-class ModuleListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ModuleListViewModel(private val repository: ModuleRepository): ViewModel() {
+
+    private val _modules = repository.getAll()
+    val modules: LiveData<List<Module>>
+        get() = _modules.asLiveData()
+
 }
