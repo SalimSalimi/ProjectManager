022b72c150ba12d885e638f4d631a337eaf84d3d
Salim Salimi <salim.salimi@protonmail.com>
2020-12-28 16:39:24 +0100

    Preparing tests for ModuleViewModel

---
+++ app/src/test/java/fr/im/salimi/projectmanager/data/repositories/FakeModuleDataSource.kt
@@ -0,0 +1,33 @@
+package fr.im.salimi.projectmanager.data.repositories
+
+import fr.im.salimi.projectmanager.data.daos.BaseDao
+import fr.im.salimi.projectmanager.data.entities.Module
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class FakeModuleDataSource(var modulesList: MutableList<Module> = mutableListOf()):BaseDao<Module> {
+    override suspend fun insert(entity: Module) {
+        modulesList.add(entity)
+    }
+
+    override suspend fun delete(entity: Module) {
+        modulesList.remove(entity)
+    }
+
+    override suspend fun update(entity: Module) {
+
+    }
+
+    override suspend fun getById(id: Long): Module {
+        val module = modulesList.filter {
+            it.id == id
+        }
+        return module[0]
+    }
+
+    override fun getAll(): Flow<List<Module>> {
+        return flow<List<Module>> {
+            emit(modulesList)
+        }
+    }
+}
---
+++ app/src/test/java/fr/im/salimi/projectmanager/data/repositories/ModuleRepositoryTest.kt
@@ -0,0 +1,19 @@
+package fr.im.salimi.projectmanager.data.repositories
+
+import fr.im.salimi.projectmanager.data.entities.Module
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@ExperimentalCoroutinesApi
+class ModuleRepositoryTest(private val fakeModuleDataSource: FakeModuleDataSource):BaseRepository<Module>(fakeModuleDataSource) {
+
+    fun initRepository() {
+        val module1 = Module(name = "Module1", description = "description1")
+        val module2 = Module(name = "Module2", description = "description2")
+        val module3 = Module(name = "Module3", description = "description3")
+
+        val list = mutableListOf<Module>()
+        list.add(module1)
+        list.add(module2)
+        list.add(module3)
+    }
+}
