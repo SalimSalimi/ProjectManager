9756b87b97f9343d9567d87be98d12c1bfbba6b3
Salim Salimi <salim.salimi@protonmail.com>
2020-12-28 18:08:07 +0100

    Building UI project_form_fragment and its ViewModel

--- app/src/main/java/fr/im/salimi/projectmanager/ui/moduleForm/ModuleFormViewModel.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/moduleForm/ModuleFormViewModel.kt
@@ -1,7 +1,25 @@
 package fr.im.salimi.projectmanager.ui.moduleForm
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import fr.im.salimi.projectmanager.data.entities.Module
 
 class ModuleFormViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _module = MutableLiveData<Module>()
+    val module: LiveData<Module>
+        get() = _module
+
+    init {
+        _module.value = Module()
+    }
+
+    fun onAddBtnClicked() {
+
+    }
+
+    fun onDateClickedEvent() {
+
+    }
 }
