668ed3b5773c61b8ac551ccb965d5e129ac18419
Salim Salimi <salim.salimi@protonmail.com>
2020-12-29 00:53:45 +0100

    Fix some XML issues

--- app/src/main/java/fr/im/salimi/projectmanager/ui/moduleList/ModuleListFragment.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/moduleList/ModuleListFragment.kt
@@ -4,27 +4,50 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import fr.im.salimi.projectmanager.R
+import fr.im.salimi.projectmanager.data.database.ProjectRoomDatabase
+import fr.im.salimi.projectmanager.data.repositories.ModuleRepository
+import fr.im.salimi.projectmanager.databinding.ModuleListFragmentBinding
 
 class ModuleListFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ModuleListFragment()
+    private lateinit var moduleAdapter: ModuleListAdapter
+    private lateinit var binding: ModuleListFragmentBinding
+    private val viewModel: ModuleListViewModel by viewModels {
+        val database = ProjectRoomDatabase.getInstance(requireContext())
+        val dao = database.moduleDao()
+        ModuleListViewModelFactory(ModuleRepository(dao))
     }
 
-    private lateinit var viewModel: ModuleListViewModel
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.module_list_fragment, container, false)
+                              savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.module_list_fragment, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ModuleListViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.viewModel = viewModel
+        initAdapter()
+        initObservers()
     }
 
+    private fun initObservers() {
+        viewModel.modules.observe(viewLifecycleOwner) {
+            moduleAdapter.submitList(it)
+        }
+    }
+
+    private fun initAdapter() {
+        binding.modulesList.apply {
+            val layoutManager = LinearLayoutManager(requireContext())
+            this.layoutManager = layoutManager
+            moduleAdapter = ModuleListAdapter()
+            this.adapter = moduleAdapter
+        }
+    }
 }
