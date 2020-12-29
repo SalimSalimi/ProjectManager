a5fab97527f9217401b0f2678526bbec47318798
Salim Salimi <salim.salimi@protonmail.com>
2020-12-29 17:28:52 +0100

    Using fab Btn to add new module from ModuleListFragment.kt

--- app/src/main/java/fr/im/salimi/projectmanager/MainActivity.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/MainActivity.kt
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bottomNavigation: BottomNavigationView
+    private lateinit var fabBtn: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //setSupportActionBar(findViewById(R.id.toolbar))
         bottomNavigation = findViewById(R.id.bottom_navigation_main)
+        fabBtn = findViewById(R.id.fab_main)
+
         bottomNavigation.menu.getItem(2).isEnabled = false
         bottomNavigation.background = null
         val navController = findNavController(R.id.nav_host_fragment)
