02846f3c701dce1b7f1f506db262ccc9e64d8c8c
Salim Salimi <salim.salimi@protonmail.com>
2020-12-29 14:11:58 +0100

    Adding a fab button to bottombar

--- app/src/main/java/fr/im/salimi/projectmanager/MainActivity.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/MainActivity.kt
@@ -1,8 +1,6 @@
 package fr.im.salimi.projectmanager
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -17,24 +15,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //setSupportActionBar(findViewById(R.id.toolbar))
         bottomNavigation = findViewById(R.id.bottom_navigation_main)
+        bottomNavigation.menu.getItem(2).isEnabled = false
+        bottomNavigation.background = null
         val navController = findNavController(R.id.nav_host_fragment)
 
         bottomNavigation.setupWithNavController(navController)
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
