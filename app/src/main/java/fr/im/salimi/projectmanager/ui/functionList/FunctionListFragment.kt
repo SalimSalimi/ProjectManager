fe870c6aafd292f114835e5d5a49ba99d3abd5f5
Salim Salimi <salim.salimi@protonmail.com>
2020-12-30 23:59:57 +0100

    Connecting FunctionlistViewModel, FunctionListFragment.kt and FunctionlistAdapter with all to show list of functions

--- app/src/main/java/fr/im/salimi/projectmanager/ui/functionList/FunctionListFragment.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/functionList/FunctionListFragment.kt
@@ -4,22 +4,71 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import fr.im.salimi.projectmanager.R
+import fr.im.salimi.projectmanager.data.database.ProjectRoomDatabase
+import fr.im.salimi.projectmanager.data.entities.Function
+import fr.im.salimi.projectmanager.data.repositories.FunctionRepository
+import fr.im.salimi.projectmanager.databinding.FunctionListFragmentBinding
+import fr.im.salimi.projectmanager.ui.uiUtils.ClickListenersCallback
+import fr.im.salimi.projectmanager.ui.uiUtils.FabButtonStates
+import fr.im.salimi.projectmanager.ui.uiUtils.setFabBtnBehaviour
 
-class FunctionListFragment : Fragment() {
+class FunctionListFragment : Fragment(), ClickListenersCallback<Function> {
 
-    private lateinit var viewModel: FunctionListViewModel
+    private lateinit var myAdapter: FunctionListAdapter
+    private lateinit var binding: FunctionListFragmentBinding
+    private val viewModel: FunctionListViewModel by viewModels {
+        val database = ProjectRoomDatabase.getInstance(requireContext())
+        val repository = FunctionRepository(database.functionDao())
+        FunctionListViewModelFactory(repository)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.function_list_fragment, container, false)
+                              savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.function_list_fragment, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(FunctionListViewModel::class.java)
+        binding.viewModel = viewModel
+        myAdapter = FunctionListAdapter(this)
+
+        initObservers()
+        initRecycler()
+        fabBtnClick()
     }
 
+    private fun initObservers() {
+        viewModel.functionsList.observe(viewLifecycleOwner) {
+            myAdapter.submitList(it)
+        }
+    }
+
+    private fun initRecycler() {
+        binding.functionsList.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = myAdapter
+        }
+    }
+
+    private fun fabBtnClick() {
+        setFabBtnBehaviour(FabButtonStates.PRIMARY_STATE) {
+
+        }
+    }
+
+    override fun onClick(view: View, entity: Function) {
+        //Add something here
+    }
+
+    override fun onLongClick(view: View, entity: Function): Boolean {
+        //Add Navigation
+        return true
+    }
 }
