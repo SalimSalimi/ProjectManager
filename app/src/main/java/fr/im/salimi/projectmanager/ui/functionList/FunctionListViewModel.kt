60b57aa84c15575c9999821f8d76971beee28fd3
Salim Salimi <salim.salimi@protonmail.com>
2020-12-30 23:47:42 +0100

    Implementing FunctionListViewModel.kt to get list of functions

--- app/src/main/java/fr/im/salimi/projectmanager/ui/functionList/FunctionListViewModel.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/functionList/FunctionListViewModel.kt
@@ -1,7 +1,14 @@
 package fr.im.salimi.projectmanager.ui.functionList
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import fr.im.salimi.projectmanager.data.entities.Function
+import fr.im.salimi.projectmanager.data.repositories.FunctionRepository
+class FunctionListViewModel(private val functionRepository: FunctionRepository) : ViewModel() {
 
-class FunctionListViewModel : ViewModel() {
+    private val _functionsList = functionRepository.getAll()
+    val functionsList: LiveData<List<Function>>
+        get() = _functionsList.asLiveData()
 
 }
