b07676154b9c9161f13ae49081c99da14f3e2951
Salim Salimi <salim.salimi@protonmail.com>
2020-12-30 18:11:30 +0100

    Create Function entity

---
+++ app/src/main/java/fr/im/salimi/projectmanager/data/entities/Function.kt
@@ -0,0 +1,22 @@
+package fr.im.salimi.projectmanager.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "functions")
+class Function(@PrimaryKey(autoGenerate = true)
+               @ColumnInfo(name = "module_id")
+               val functionId: Long = 0L,
+               var name: String = "",
+               var description: String = "",
+               @ColumnInfo(name = "starting_date")
+               var startingDate: Date = Date(),
+               @ColumnInfo(name = "finishing_date")
+               var endingDate: Date = Date(),
+               @ColumnInfo(name = "project_id")
+               var projectId: Long = 0L
+): BaseEntity(
+        functionId) {
+}
