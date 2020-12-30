22c8525538c6d7eb9ae0295b17166cdb5e3de565
Salim Salimi <salim.salimi@protonmail.com>
2020-12-30 18:27:35 +0100

    add FunctionFormFragment with its ViewModel

---
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/functionForm/FunctionFormFragment.kt
@@ -0,0 +1,27 @@
+package fr.im.salimi.projectmanager.ui.functionForm
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import fr.im.salimi.projectmanager.R
+
+class FunctionFormFragment : Fragment() {
+
+    private lateinit var viewModel: FunctionFormViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.function_form_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(FunctionFormViewModel::class.java)
+    }
+
+}
---
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/functionForm/FunctionFormViewModel.kt
@@ -0,0 +1,7 @@
+package fr.im.salimi.projectmanager.ui.functionForm
+
+import androidx.lifecycle.ViewModel
+
+class FunctionFormViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
