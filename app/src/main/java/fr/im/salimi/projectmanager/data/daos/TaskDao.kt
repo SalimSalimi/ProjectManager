2ae6953c90e7fd357f7012df13943ab2e0e6408a
Salim Salimi <salim.salimi@protonmail.com>
2021-01-02 18:24:41 +0100

    Add tasks to the database

--- app/src/main/java/fr/im/salimi/projectmanager/data/daos/TaskDao.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/data/daos/TaskDao.kt
@@ -1,9 +1,11 @@
 package fr.im.salimi.projectmanager.data.daos
 
+import androidx.room.Dao
 import androidx.room.Query
 import fr.im.salimi.projectmanager.data.entities.Task
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 abstract class TaskDao: BaseDao<Task> {
 
     @Query("SELECT * FROM tasks")
