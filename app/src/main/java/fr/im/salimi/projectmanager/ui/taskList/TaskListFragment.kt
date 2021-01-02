07dfaaadc2180a155ebfdb6124129385e5c3abd1
Salim Salimi <salim.salimi@protonmail.com>
2021-01-02 23:26:20 +0100

    Show list of Tasks on TaskListFragment

--- app/src/main/java/fr/im/salimi/projectmanager/ui/taskList/TaskListFragment.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/taskList/TaskListFragment.kt
@@ -4,29 +4,86 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import fr.im.salimi.projectmanager.R
+import fr.im.salimi.projectmanager.data.database.ProjectRoomDatabase
+import fr.im.salimi.projectmanager.data.entities.Task
+import fr.im.salimi.projectmanager.data.repositories.TaskRepository
+import fr.im.salimi.projectmanager.databinding.TaskListFragmentBinding
+import fr.im.salimi.projectmanager.ui.functionList.FunctionListFragmentDirections
+import fr.im.salimi.projectmanager.ui.uiUtils.ClickListenersCallback
 
-class TaskListFragment : Fragment() {
+class TaskListFragment : Fragment(), ClickListenersCallback<Task> {
 
-    companion object {
-        fun newInstance() = TaskListFragment()
+    private lateinit var myAdapter: TaskListAdapter
+    private lateinit var binding: TaskListFragmentBinding
+    private val viewModel: TaskListViewModel by viewModels {
+        val database = ProjectRoomDatabase.getInstance(requireContext())
+        val dao = database.taskDao()
+        TaskListViewModelFactory(TaskRepository(dao))
     }
-
-    private lateinit var viewModel: TaskListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.task_list_fragment, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.task_list_fragment, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(TaskListViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.viewModel = viewModel
+
+        initRecycler()
+        initObservers()
+    }
+
+    private fun initObservers() {
+        viewModel.tasksList.observe(viewLifecycleOwner) {
+            myAdapter.submitList(it)
+        }
+
+        /*viewModel.navigateToFunctionFormEvent.observe(viewLifecycleOwner) {
+            if (it) {
+                navigateToFunctionForm(sendingID)
+                viewModel.navigateToFunctionFormEventDone()
+            }
+        }*/
+    }
+
+    private fun initRecycler() {
+        myAdapter = TaskListAdapter(this)
+        binding.tasksList.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = myAdapter
+        }
+    }
+
+    /*private fun fabBtnClick() {
+        setFabBtnBehaviour(FabButtonStates.PRIMARY_STATE) {
+            sendingID = -1L
+            viewModel.navigateToFunctionFormEventTriggered()
+        }
+    }*/
+
+    private fun navigateToFunctionForm(id: Long) {
+        val direction =
+            FunctionListFragmentDirections.actionFunctionListFragmentToFunctionFormFragment(id)
+        this.findNavController().navigate(direction)
+    }
+
+    override fun onClick(view: View, entity: Task) {
+
+    }
+
+    override fun onLongClick(view: View, entity: Task): Boolean {
+        return true
     }
 
 }
--- app/src/main/java/fr/im/salimi/projectmanager/ui/taskList/TaskListViewModel.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/taskList/TaskListViewModel.kt
@@ -9,6 +9,6 @@ import fr.im.salimi.projectmanager.data.repositories.TaskRepository
 class TaskListViewModel(private val repository: TaskRepository) : ViewModel() {
 
     private val _tasksList = repository.getAll()
-    val tasks: LiveData<List<Task>>
+    val tasksList: LiveData<List<Task>>
         get() = _tasksList.asLiveData()
 }
