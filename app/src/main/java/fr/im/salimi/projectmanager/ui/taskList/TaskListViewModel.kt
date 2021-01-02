93ec09065acb4923f296d631227dc175fd502353
Salim Salimi <salim.salimi@protonmail.com>
2021-01-02 22:38:33 +0100

    Implement TasksViewModel to get list of tasks

--- app/src/main/java/fr/im/salimi/projectmanager/ui/taskList/TaskListViewModel.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/taskList/TaskListViewModel.kt
@@ -1,7 +1,14 @@
 package fr.im.salimi.projectmanager.ui.taskList
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import fr.im.salimi.projectmanager.data.entities.Task
+import fr.im.salimi.projectmanager.data.repositories.TaskRepository
 
-class TaskListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class TaskListViewModel(private val repository: TaskRepository) : ViewModel() {
+
+    private val _tasksList = repository.getAll()
+    val tasks: LiveData<List<Task>>
+        get() = _tasksList.asLiveData()
 }
