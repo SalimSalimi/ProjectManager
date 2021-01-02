7ed77857c9a94ec6e1d57243e2ae83693400cdd9
Salim Salimi <salim.salimi@protonmail.com>
2021-01-02 22:55:05 +0100

    Create TaskListViewModelFactory.kt to instantiate TaskViewModel

---
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/taskList/TaskListViewModelFactory.kt
@@ -0,0 +1,15 @@
+package fr.im.salimi.projectmanager.ui.taskList
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import fr.im.salimi.projectmanager.data.repositories.TaskRepository
+
+@Suppress("UNCHECKED_CAST")
+class TaskListViewModelFactory(private val taskRepository: TaskRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(TaskListViewModel::class.java)) {
+            return TaskListViewModel(taskRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
+}
