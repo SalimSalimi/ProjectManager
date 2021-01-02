4f4a7a9087bf548ca45e698433e3f50b4029893c
Salim Salimi <salim.salimi@protonmail.com>
2021-01-02 18:21:18 +0100

    Defining task entity

---
+++ app/src/main/java/fr/im/salimi/projectmanager/data/entities/Task.kt
@@ -0,0 +1,20 @@
+package fr.im.salimi.projectmanager.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
+import java.util.*
+
+class Task(
+        @PrimaryKey(autoGenerate = true)
+        @ColumnInfo(name = "task_id")
+        val taskId: Long = 0L,
+        var name: String = "",
+        var description: String = "",
+        @ColumnInfo(name = "starting_date")
+        var startingDate: Date = Date(),
+        @ColumnInfo(name = "finishing_date")
+        var endingDate: Date = Date(),
+        @ColumnInfo(name = "function_id")
+        var functionId: Long = 0L
+) : BaseEntity(taskId) {
+}
