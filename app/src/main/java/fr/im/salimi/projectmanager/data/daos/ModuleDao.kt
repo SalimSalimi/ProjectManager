6bec2ee09b18913986d85229115ed3c6e7ce71ac
Salim Salimi <salim.salimi@protonmail.com>
2021-01-03 22:43:10 +0100

    Create queries getByIdWithFunctiond & getAllWithFunctions to ModuleDao

--- app/src/main/java/fr/im/salimi/projectmanager/data/daos/ModuleDao.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/data/daos/ModuleDao.kt
@@ -3,6 +3,7 @@ package fr.im.salimi.projectmanager.data.daos
 import androidx.room.Dao
 import androidx.room.Query
 import fr.im.salimi.projectmanager.data.entities.Module
+import fr.im.salimi.projectmanager.data.entities.relations.ModuleWithFunctions
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -13,4 +14,10 @@ abstract class ModuleDao: BaseDao<Module> {
 
     @Query("SELECT * FROM modules")
     abstract override fun getAll(): Flow<List<Module>>
+
+    @Query("SELECT * FROM modules")
+    abstract fun getAllWithFunctions(): Flow<List<ModuleWithFunctions>>
+
+    @Query("SELECT * FROM modules where module_id = :id")
+    abstract fun getByIdWithFunctions(id: Long): Flow<List<ModuleWithFunctions>>
 }
