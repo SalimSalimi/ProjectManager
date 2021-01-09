93a19eade525846195fdf6385bc398934bd68999
Salim Salimi <salim.salimi@protonmail.com>
2021-01-09 15:36:23 +0100

    Adding TaskAssignment entity to Db and write some tests for it

--- app/src/main/java/fr/im/salimi/projectmanager/data/daos/DeveloperDao.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/data/daos/DeveloperDao.kt
@@ -1,7 +1,11 @@
 package fr.im.salimi.projectmanager.data.daos
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
 import fr.im.salimi.projectmanager.data.entities.Developer
+import fr.im.salimi.projectmanager.data.entities.TaskAssignments
+import fr.im.salimi.projectmanager.data.entities.relations.DeveloperWithTasks
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -16,4 +20,9 @@ abstract class DeveloperDao: BaseDao<Developer> {
     @Query("DELETE FROM developers")
     abstract suspend fun deleteAll()
 
+    @Insert
+    abstract fun assignTask(taskAssignments: TaskAssignments)
+
+    @Query("SELECT * FROM developers WHERE developer_id =:id")
+    abstract suspend fun getDeveloperWithTasksById(id: Long): DeveloperWithTasks
 }
--- app/src/main/java/fr/im/salimi/projectmanager/data/daos/TaskDao.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/data/daos/TaskDao.kt
@@ -3,6 +3,7 @@ package fr.im.salimi.projectmanager.data.daos
 import androidx.room.Dao
 import androidx.room.Query
 import fr.im.salimi.projectmanager.data.entities.Task
+import fr.im.salimi.projectmanager.data.entities.relations.TaskWithDevelopers
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -13,4 +14,7 @@ abstract class TaskDao: BaseDao<Task> {
 
     @Query("SELECT * FROM tasks WHERE task_id = :id")
     abstract override suspend fun getById(id: Long): Task
+
+    @Query("SELECT * FROM tasks WHERE task_id = :id")
+    abstract suspend fun getTaskWithDevelopersByTaskId(id: Long): TaskWithDevelopers
 }
