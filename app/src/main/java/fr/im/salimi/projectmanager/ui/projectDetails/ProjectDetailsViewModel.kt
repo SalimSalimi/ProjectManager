6656997a70ba3cf89c3ef119e74c85ce9bbb9b68
Salim Salimi <salim.salimi@protonmail.com>
2021-01-13 11:53:29 +0100

    get project by id from viewmodel and implement VM factory

--- app/src/main/java/fr/im/salimi/projectmanager/ui/projectDetails/ProjectDetailsViewModel.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/projectDetails/ProjectDetailsViewModel.kt
@@ -1,7 +1,26 @@
 package fr.im.salimi.projectmanager.ui.projectDetails
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import fr.im.salimi.projectmanager.data.entities.Project
+import fr.im.salimi.projectmanager.data.repositories.ProjectRepository
+import kotlinx.coroutines.launch
 
-class ProjectDetailsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ProjectDetailsViewModel(private val id: Long, private val projectRepository: ProjectRepository) : ViewModel() {
+
+    private val _project = MutableLiveData<Project>()
+    val project: LiveData<Project>
+        get() = _project
+
+    init {
+        initProject()
+    }
+
+    private fun initProject() {
+        viewModelScope.launch {
+            _project.value = projectRepository.getById(id)
+        }
+    }
 }
