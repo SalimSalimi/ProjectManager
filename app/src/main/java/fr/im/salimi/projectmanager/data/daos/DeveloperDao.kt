be7447e8924f5b1fe15e3f4aeb7270477ad3a12b
Salim Salimi <salim.salimi@protonmail.com>
2021-01-14 01:56:10 +0100

    reformat getDevelopersByProject query

--- app/src/main/java/fr/im/salimi/projectmanager/data/daos/DeveloperDao.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/data/daos/DeveloperDao.kt
@@ -15,7 +15,13 @@ abstract class DeveloperDao: BaseDao<Developer> {
     @Query("SELECT * FROM developers WHERE developer_id = :id")
     abstract override suspend fun getById(id: Long): Developer
 
-    @Query("SELECT d.developer_id, d.first_name, d.last_name, d.city, d.country, d.email, d.phone_number, d.post, d.postalCode, d.roadName, d.roadNumber FROM developers d JOIN tasks_assignments ta ON d.developer_id = ta.task_id JOIN tasks t ON t.task_id = ta.task_id JOIN functions f ON f.function_id = t.task_id JOIN modules m ON m.module_id = f.module_id_fk AND m.project_id_fk = :projectId")
+    @Query("SELECT d.developer_id, d.first_name, d.last_name, d.city, d.country, d.email, d.phone_number, " +
+            "d.post, d.postalCode, d.roadName, d.roadNumber " +
+            "FROM developers d JOIN tasks_assignments ta ON d.developer_id = ta.task_id " +
+            "JOIN tasks t ON t.task_id = ta.task_id " +
+            "JOIN functions f ON f.function_id = t.task_id " +
+            "JOIN modules m ON m.module_id = f.module_id_fk " +
+            "AND m.project_id_fk = :projectId")
     abstract fun getDevelopersByProject(projectId: Long): Flow<List<Developer>>
 
     @Query("DELETE FROM developers")
