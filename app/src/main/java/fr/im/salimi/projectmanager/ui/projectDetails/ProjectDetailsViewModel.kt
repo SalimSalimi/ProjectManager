34e7b2276973da5b2c30f096d5b17bfb224caaed
Salim Salimi <salim.salimi@protonmail.com>
2021-01-17 12:46:12 +0100

    show project state in ProjectDetailsFragment.kt

--- app/src/main/java/fr/im/salimi/projectmanager/ui/projectDetails/ProjectDetailsViewModel.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/ui/projectDetails/ProjectDetailsViewModel.kt
@@ -1,18 +1,21 @@
 package fr.im.salimi.projectmanager.ui.projectDetails
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import fr.im.salimi.projectmanager.data.entities.Project
+import androidx.lifecycle.*
+import fr.im.salimi.projectmanager.data.entities.subsets.ProjectState
 import fr.im.salimi.projectmanager.data.repositories.ProjectRepository
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
 
 class ProjectDetailsViewModel(private val id: Long, private val projectRepository: ProjectRepository) : ViewModel() {
 
-    private val _project = MutableLiveData<Project>()
-    val project: LiveData<Project>
-        get() = _project
+    private val _projectState: Flow<ProjectState> = flow {
+        if (id != -1L)
+            emitAll(projectRepository.getProjectStateById(id))
+    }
+    val projectState: LiveData<ProjectState> = Transformations.map(_projectState.asLiveData()) {
+        it
+    }
 
     private val _getProjectIsDone = MutableLiveData<Boolean>()
     val getProjectIsDone: LiveData<Boolean>
@@ -37,7 +40,6 @@ class ProjectDetailsViewModel(private val id: Long, private val projectRepositor
 
     init {
         initEvents()
-        initProject()
     }
     
     fun onGetProjectDone() {
@@ -74,14 +76,6 @@ class ProjectDetailsViewModel(private val id: Long, private val projectRepositor
 
     fun onDevelopersClicked() {
         _developersClickEvent.value = false
-    }
-
-
-    private fun initProject() {
-        viewModelScope.launch {
-            _project.value = projectRepository.getById(id)
-            _getProjectIsDone.value = true
-        }
     }
 
     private fun initEvents() {
