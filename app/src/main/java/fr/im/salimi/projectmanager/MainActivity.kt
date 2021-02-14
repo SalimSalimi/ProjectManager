5f2e5c595b15f9dbdf7b21ee50d3d01d1efa086e
Salim Salimi <salim.salimi@protonmail.com>
2021-02-14 18:22:26 +0100

    Create an alarm to trigger a notification

--- app/src/main/java/fr/im/salimi/projectmanager/MainActivity.kt
+++ app/src/main/java/fr/im/salimi/projectmanager/MainActivity.kt
@@ -1,16 +1,22 @@
 package fr.im.salimi.projectmanager
 
+import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.AlarmManagerCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import fr.im.salimi.projectmanager.data.database.ProjectRoomDatabase
+import fr.im.salimi.projectmanager.receivers.NotificationAlarmReceiver
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,6 +38,8 @@ class MainActivity : AppCompatActivity() {
         ProjectRoomDatabase.createInstance(applicationContext)
         createNotificationChannel(applicationContext.getString(R.string.notification_tasks_channel_id),
                 applicationContext.getString(R.string.notification_tasks_name))
+
+        createNotificationAlarm()
     }
 
     private fun createNotificationChannel(channelID: String, channelDescription: String) {
@@ -50,5 +58,24 @@ class MainActivity : AppCompatActivity() {
             val notificationManager = applicationContext.getSystemService(NotificationManager::class.java) as NotificationManager
             notificationManager.createNotificationChannel(notificationChannel)
         }
+    }
+
+    private fun createNotificationAlarm() {
+        val alarmManger = applicationContext.getSystemService(ALARM_SERVICE) as AlarmManager
+        val notificationBroadcast = Intent(this, NotificationAlarmReceiver::class.java)
+
+        val pendingIntent = PendingIntent.getBroadcast(applicationContext,
+                0,
+                notificationBroadcast,
+                PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val triggerTime = SystemClock.elapsedRealtime() + 10_000L
+
+        AlarmManagerCompat.setExactAndAllowWhileIdle(
+                alarmManger,
+                AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                triggerTime,
+                pendingIntent
+        )
     }
 }
